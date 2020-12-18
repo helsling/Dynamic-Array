aaca62286cc972e4f0d4141de589f7b802adec2c
pwillsmaith <pwillsmaith@gmail.com>
2020-12-18 18:33:35 -0500

    implement insertion sort

--- src/com/company/Dynamic.java
+++ src/com/company/Dynamic.java
@@ -79,4 +79,16 @@ public class Dynamic {
     public void setSizeOfArray(int sizeOfArray) {
         this.sizeOfArray = sizeOfArray;
     }
+    public void sort(){
+        for( int i = 1; i < array.length; i ++){
+            int current = array[i];
+            int j = i - 1;
+            while( j >= 0 && array[j] > current ){
+                array[j+1] = array[j];
+                j--;
+            }
+            array[j+1] = current;
+        }
+    }
+
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,12 +4,15 @@ public class Main {
 
     public static void main(String[] args) {
         // write your code here
-        Dynamic list = new Dynamic(2);
-        list.add(1,0);
-        list.add (2, 1);
-        list.append(4);
+        Dynamic list = new Dynamic();
         list.append(5);
+        list.append(3);
+        list.append(6);
+        list.append(1);
         System.out.println(list.getSizeOfArray());
         System.out.println(list.toString());
+        list.sort();
+        System.out.println(list.toString());
+
     }
 }
